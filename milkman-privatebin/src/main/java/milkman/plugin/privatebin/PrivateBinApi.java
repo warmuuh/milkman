597a4e56c98828a8890fc2f7bb3e7f90f4af21a7
Peter Mucha <peter.mucha@sap.com>
2019-04-25 12:26:43 +0200

    improved privateBin integration securitywise

--- milkman-privatebin/src/main/java/milkman/plugin/privatebin/PrivateBinApi.java
+++ milkman-privatebin/src/main/java/milkman/plugin/privatebin/PrivateBinApi.java
@@ -1,16 +1,27 @@
 package milkman.plugin.privatebin;
 
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
 import java.io.OutputStream;
 import java.net.HttpURLConnection;
 import java.net.URL;
 import java.net.URLEncoder;
 import java.security.SecureRandom;
+import java.security.spec.InvalidKeySpecException;
 import java.util.Base64;
+import java.util.zip.Deflater;
+import java.util.zip.DeflaterOutputStream;
+import java.util.zip.Inflater;
+import java.util.zip.InflaterInputStream;
 
 import javax.crypto.Cipher;
 import javax.crypto.SecretKey;
+import javax.crypto.SecretKeyFactory;
 import javax.crypto.spec.GCMParameterSpec;
+import javax.crypto.spec.PBEKeySpec;
 import javax.crypto.spec.SecretKeySpec;
+
+import org.apache.commons.io.IOUtils;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
@@ -21,20 +32,22 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import lombok.RequiredArgsConstructor;
-import milkman.persistence.UnknownPluginHandler;
+import lombok.extern.slf4j.Slf4j;
 
 /**
- * todo: how to use salt?
- * 
- * @author peter
+ * todo: make it work with browser-client of privateBin (we cant use this because we apparently use a slightly different
+ *  way of encryption. cant find the issue though
  *
  */
 @RequiredArgsConstructor
+@Slf4j
 public class PrivateBinApi {
+	private static final int ITERATIONS = 10000;
+	private static final int AES_KEY_LENGTH = 256;
+	private static final int AUTH_TAG_LENGTH = 128;
 	private final String privateBinHost;
 	static SecureRandom secureRandom = new SecureRandom();
-	
-	
+
 	@Data
 	@JsonIgnoreProperties(ignoreUnknown = true)
 	public static class PrivateBinResponse {
@@ -43,7 +56,7 @@ public class PrivateBinApi {
 		private int status;
 		private String data;
 	}
-	
+
 	@Data
 	@NoArgsConstructor
 	@AllArgsConstructor
@@ -54,7 +67,7 @@ public class PrivateBinApi {
 
 		@JsonProperty("v")
 		private int version;
-		
+
 		@JsonProperty("iter")
 		private int iterations;
 
@@ -69,180 +82,175 @@ public class PrivateBinApi {
 
 		@JsonProperty("adata")
 		private String adata;
-		
-		@JsonProperty("cypher")
-		private String cypher;
+
+		@JsonProperty("cipher")
+		private String cipher;
 
 		@JsonProperty("salt")
 		private String salt;
-		
+
 		@JsonProperty("ct")
 		private String content;
-		
+
 		@JsonIgnore
 		private String secret;
 	}
-	
+
 	public String createPaste(String content, boolean burnAfterReading) throws Exception {
 		PrivateBinData encrypted = encrypt(content);
 		String body = getUrlEncodedRequestBody(encrypted, burnAfterReading);
 		String id = sendRequest(body);
 		return privateBinHost + id + "#" + encrypted.getSecret();
 	}
-	
-	
+
 	public String readPaste(String url) throws Exception {
-		String secret64 = url.split("#")[1];
-		PrivateBinData pasteData = getPasteData(url);
+		String[] split = url.split("#");
+		String secret64 = split[1];
+		PrivateBinData pasteData = getPasteData(split[0]);
 		return decrypt(pasteData, secret64);
 	}
-	
-	
+
 	private PrivateBinData getPasteData(String url) throws Exception {
 		URL myURL = new URL(url);
-		HttpURLConnection con = (HttpURLConnection)myURL.openConnection();
+		HttpURLConnection con = (HttpURLConnection) myURL.openConnection();
 
 		con.setRequestMethod("GET");
-		con.setRequestProperty ("X-Requested-With", "JSONHttpRequest");
-		
+		con.setRequestProperty("X-Requested-With", "JSONHttpRequest");
+
 		con.setUseCaches(false);
 		con.setDoInput(true);
 		con.setDoOutput(false);
-		
+
 		PrivateBinResponse response = new ObjectMapper().readValue(con.getInputStream(), PrivateBinResponse.class);
 		if (response.getStatus() != 0) {
 			throw new Exception("failed to read privatebin paste: " + response.getMessage());
 		}
-		
+
 		PrivateBinData data = new ObjectMapper().readValue(response.getData(), PrivateBinData.class);
 
 		return data;
 	}
-	
+
 	private String sendRequest(String body) throws Exception {
 		URL myURL = new URL(privateBinHost);
-		HttpURLConnection con = (HttpURLConnection)myURL.openConnection();
+		HttpURLConnection con = (HttpURLConnection) myURL.openConnection();
 
 		con.setRequestMethod("POST");
-		con.setRequestProperty ("X-Requested-With", "JSONHttpRequest");
+		con.setRequestProperty("X-Requested-With", "JSONHttpRequest");
 		con.setRequestProperty("Content-Type", "application/x-www-form-urlencoded");
 		con.setRequestProperty("Content-Length", "" + body.getBytes().length);
-		
+
 		con.setUseCaches(false);
 		con.setDoInput(true);
 		con.setDoOutput(true);
-		
+
 		OutputStream os = con.getOutputStream();
-		os.write( body.getBytes() );    
+		os.write(body.getBytes());
 		os.close();
-		
-		
+
 		PrivateBinResponse response = new ObjectMapper().readValue(con.getInputStream(), PrivateBinResponse.class);
 		if (response.getStatus() != 0) {
-			throw new Exception("failed to create privatebin paste: " + response.getMessage() + " (" + response.getStatus() + ")");
+			throw new Exception(
+					"failed to create privatebin paste: " + response.getMessage() + " (" + response.getStatus() + ")");
 		}
 		return response.getId();
 	}
-	
+
 	private static String getUrlEncodedRequestBody(PrivateBinData data, boolean burnAfterReading) throws Exception {
-//		String jsonData = new ObjectMapper().writeValueAsString(data);
-		String jsonData = "{"
-				+ "\"iv\":\""+data.getInitializationVector()+"\","
-				+ "\"v\":1,"
-				+ "\"iter\":10000,"
-				+ "\"ks\":256,"
-				+ "\"ts\":128,"
-				+ "\"mode\":\"gcm\","
-				+ "\"adata\":\"\","
-				+ "\"cipher\":\"aes\","
-				+ "\"salt\":\""+data.getSalt()+"\","
-				+ "\"ct\":\""+data.getContent()+"\"}"; 
-				
-		String encodedData = URLEncoder.encode(jsonData); 
+		String jsonData = new ObjectMapper().writeValueAsString(data);
+		String encodedData = URLEncoder.encode(jsonData);
+
+		return "data=" + encodedData + "&expire=1week" + "&formatter=plaintext" + "&burnafterreading="
+				+ (burnAfterReading ? "1" : "0") + "&opendiscussion=0";
+	}
+
+	private static PrivateBinData encrypt(String strToEncrypt) throws Exception {
+		byte[] iv = randomInitializationVector();
+		byte[] salt = generateRandomSalt();
+		byte[] key = generateRandomKey();
+		SecretKey secretKey = generateDerivedkey(key, salt);
+
+		Cipher cipher = Cipher.getInstance("AES/GCM/NoPadding");
+		GCMParameterSpec parameterSpec = new GCMParameterSpec(AUTH_TAG_LENGTH, iv);
+		cipher.init(Cipher.ENCRYPT_MODE, secretKey, parameterSpec);
 		
-		return "data="+encodedData
-				+ "&expire=1week"
-				+ "&formatter=plaintext"
-				+ "&burnafterreading=" + (burnAfterReading ? "1" : "0")
-				+ "&opendiscussion=0";
+		String compressedData = deflate(strToEncrypt.getBytes("UTF-8"));
+		byte[] encrypted = cipher.doFinal(compressedData.getBytes());
+		
+		return new PrivateBinData(
+				Base64.getEncoder().encodeToString(iv), 
+				1, ITERATIONS, AES_KEY_LENGTH, AUTH_TAG_LENGTH, "gcm", "", "aes",
+				Base64.getEncoder().encodeToString(salt),
+				Base64.getEncoder().encodeToString(encrypted),
+				Base64.getEncoder().encodeToString(key));
 	}
 	
-	private static PrivateBinData encrypt(String strToEncrypt)  throws Exception {
-        byte[] iv = randomInitializationVector();
-        SecretKey secretKey = generateRandomKey();
-        byte[] salt = generateRandomSalt();
-        
-        
-//        SecretKeyFactory factory = SecretKeyFactory.getInstance("PBKDF2WithHmacSHA256");
-//        KeySpec spec = new PBEKeySpec(secret.toCharArray(), salt.getBytes(), 65536, 256);
-//        SecretKey tmp = factory.generateSecret(spec);
-//        SecretKeySpec secretKey = new SecretKeySpec(tmp.getEncoded(), "AES");
-        
-//		Cipher cipher = Cipher.getInstance("AES/CBC/PKCS5PADDING");
-//		IvParameterSpec ivspec = new IvParameterSpec(iv);
-//		cipher.init(Cipher.ENCRYPT_MODE, secretKey, ivspec);
+	private static String decrypt(PrivateBinData data, String secret64) throws Exception {
+		byte[] iv = Base64.getDecoder().decode(data.getInitializationVector());
+		byte[] salt = Base64.getDecoder().decode(data.getSalt());
+		byte[] secretBytes = Base64.getDecoder().decode(secret64);
+		byte[] encrypted = Base64.getDecoder().decode(data.getContent());
 
-        
-        Cipher cipher = Cipher.getInstance("AES/GCM/NoPadding");
-        GCMParameterSpec parameterSpec = new GCMParameterSpec(128, iv);
-        cipher.init(Cipher.ENCRYPT_MODE, secretKey, parameterSpec);
-        
-        
-        return new PrivateBinData(
-        		Base64.getEncoder().encodeToString(iv),
-        		1,
-        		10000,
-        		256,
-        		128,
-        		"gcm",
-        		"",
-        		"aes",
-        		Base64.getEncoder().encodeToString(salt),
-        		Base64.getEncoder().encodeToString(cipher.doFinal(strToEncrypt.getBytes("UTF-8"))),
-        		Base64.getEncoder().encodeToString(secretKey.getEncoded())
-        		);
+		SecretKey secretKey = generateDerivedkey(secretBytes, salt);
+
+		Cipher cipher = Cipher.getInstance("AES/GCM/NoPadding");
+		GCMParameterSpec parameterSpec = new GCMParameterSpec(data.getAuthenticationTag(), iv);
+		cipher.init(Cipher.DECRYPT_MODE, secretKey, parameterSpec);
+
+		byte[] decrypted = cipher.doFinal(encrypted);
+		String inflated = new String(inflate(new String(decrypted)));
+		return inflated;
 	}
 	
-	private static SecretKey generateRandomKey() {
-		byte[] key = new byte[16];
+	private static SecretKey generateDerivedkey(byte[] key, byte[] salt) throws Exception {
+		SecretKeyFactory factory = SecretKeyFactory.getInstance("PBKDF2WithHmacSHA256");
+	    SecretKey derivedKey = factory.generateSecret(new PBEKeySpec(new String(key).toCharArray(), salt, ITERATIONS, AES_KEY_LENGTH));
+	    SecretKey secretKey = new SecretKeySpec(derivedKey.getEncoded(), "AES");
+	    return secretKey;
+	}
+
+	private static byte[] generateRandomKey() {
+		byte[] key = new byte[AES_KEY_LENGTH / 8];
 		secureRandom.nextBytes(key);
-		SecretKey secretKey = new SecretKeySpec(key, "AES");
-		return secretKey;
+		return key;
 	}
 
+	
+	
 	private static byte[] generateRandomSalt() {
 		byte[] key = new byte[8];
 		secureRandom.nextBytes(key);
 		return key;
 	}
+
 	private static byte[] randomInitializationVector() {
-		byte[] iv = new byte[12]; //NEVER REUSE THIS IV WITH SAME KEY
+		byte[] iv = new byte[12]; // NEVER REUSE THIS IV WITH SAME KEY
 		secureRandom.nextBytes(iv);
 		return iv;
 	}
-	
-	private static String decrypt(PrivateBinData data, String secret64) throws Exception {
-        byte[] iv = Base64.getDecoder().decode(data.getInitializationVector());
-        byte[] salt =  Base64.getDecoder().decode(data.getSalt());
-        
-//        SecretKeyFactory factory = SecretKeyFactory.getInstance("PBKDF2WithHmacSHA256");
-//        KeySpec spec = new PBEKeySpec(secret.toCharArray(), salt, 65536, 256);
-//        SecretKey tmp = factory.generateSecret(spec);
-//        SecretKeySpec secretKey = new SecretKeySpec(tmp.getEncoded(), "AES");
-        byte[] secretBytes =  Base64.getDecoder().decode(secret64);
-        SecretKey secretKey = new SecretKeySpec(secretBytes, "AES");
-        
-        
-//		Cipher cipher = Cipher.getInstance("AES/CBC/PKCS5PADDING");
-//		IvParameterSpec ivspec = new IvParameterSpec(iv);
-//		cipher.init(Cipher.DECRYPT_MODE, secretKey, ivspec);
-		
-        Cipher cipher = Cipher.getInstance("AES/GCM/NoPadding");
-        GCMParameterSpec parameterSpec = new GCMParameterSpec(128, iv);
-        cipher.init(Cipher.DECRYPT_MODE, secretKey, parameterSpec);
-		
-		
-        return new String(cipher.doFinal(Base64.getDecoder().decode(data.getContent())));
+
+
+
+	private static String deflate(byte[] data) throws Exception {
+		DeflaterOutputStream def = null;
+		String compressed = null;
+		ByteArrayOutputStream out = new ByteArrayOutputStream();
+		// create deflater without header
+		def = new DeflaterOutputStream(out, new Deflater(Deflater.DEFAULT_COMPRESSION, true));
+		def.write(data);
+		def.close();
+		compressed = Base64.getEncoder().encodeToString(out.toByteArray());
+		return compressed;
 	}
 	
+
+	private static byte[] inflate(String data) throws Exception {
+		byte[] compressed = Base64.getDecoder().decode(data);
+		ByteArrayInputStream in = new ByteArrayInputStream(compressed);
+		InflaterInputStream inf = new InflaterInputStream(in, new Inflater(true));
+		byte[] byteArray = IOUtils.toByteArray(inf);
+		return byteArray;
+	}
+	
+
 }
--- milkman-privatebin/src/test/java/milkman/plugin/privatebin/PrivateBinApiTest.java
+++ milkman-privatebin/src/test/java/milkman/plugin/privatebin/PrivateBinApiTest.java
@@ -8,18 +8,24 @@ import org.junit.jupiter.api.Test;
 class PrivateBinApiTest {
 
 	@Test
-	void testPasteCreation() throws Exception {
+	void testPasteDeEncryption() throws Exception {
 		PrivateBinApi sut = new PrivateBinApi("https://privatebin.net/?");
 		String id = sut.createPaste("some test content", false);
 		System.out.println("ID: " + id);
 		assertThat(id).isNotBlank();
+
+		
+		String content = sut.readPaste(id);
+		
+		System.out.println("content: " + content);
+		assertThat(content).isEqualTo("some test content");
 	}
-	
+
 	
 	@Test
 	void testPasteLookup() throws Exception {
 		PrivateBinApi sut = new PrivateBinApi("https://privatebin.net/?");
-		String content = sut.readPaste("https://privatebin.net/?b67cd2775e22d8ed#XNsfay6QBL0NFzA/KPDkLA==");
+		String content = sut.readPaste("https://privatebin.net/?05a52b2790940344#rtZTv6VVAjzrHzIKV1uHdJbnfygWmA+8eJXJ5PFzy4Y=");
 		
 		
 		System.out.println("content: " + content);
