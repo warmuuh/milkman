c16b0f3fbd2819ad7791d26d25f847ab7cc140c9
Peter Mucha <peter.mucha@sap.com>
2019-06-16 16:26:12 +0200

    setup bundled custom jre for windows

--- milkman-dist/src/bin/milkman.bat
+++ milkman-dist/src/bin/milkman.bat
@@ -1,1 +1,10 @@
-@start javaw -Dprism.dirtyopts=false -cp plugins/*;milkman.jar milkman.MilkmanApplication
+@start .\jre-win64\bin\javaw ^
+	--add-exports javafx.controls/com.sun.javafx.scene.control.behavior=ALL-UNNAMED ^
+	--add-exports javafx.controls/com.sun.javafx.scene.control=ALL-UNNAMED ^
+	--add-exports javafx.graphics/com.sun.javafx.scene=ALL-UNNAMED ^
+	--add-exports javafx.base/com.sun.javafx.event=ALL-UNNAMED ^
+	--add-opens javafx.controls/javafx.scene.control.skin=ALL-UNNAMED ^
+	--add-opens javafx.controls/javafx.scene.control=ALL-UNNAMED ^
+	--add-opens javafx.graphics/javafx.scene.text=ALL-UNNAMED ^
+	--add-opens javafx.graphics/com.sun.javafx.text=ALL-UNNAMED ^
+	-Dprism.dirtyopts=false -cp plugins/*;milkman.jar milkman.MilkmanApplication
