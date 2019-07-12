c38d2d1f1670a89b79fbadb8e22a2facefbe5663
Peter Mucha <peter.mucha@sap.com>
2019-07-12 10:54:27 +0200

    added proper exception dialog for exceptions on startup

---
+++ milkman/src/main/java/milkman/ExceptionDialog.java
@@ -0,0 +1,48 @@
+package milkman;
+
+import java.io.PrintWriter;
+import java.io.StringWriter;
+
+import javafx.scene.control.Alert;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextArea;
+import javafx.scene.layout.GridPane;
+import javafx.scene.layout.Priority;
+
+public class ExceptionDialog extends Alert {
+
+	public ExceptionDialog(Throwable ex) {
+		super(AlertType.ERROR);
+		setTitle("Exception");
+		setHeaderText("Exception during startup of application");
+		setContentText(ex.getClass().getName() + ": " + ex.getMessage());
+
+
+		// Create expandable Exception.
+		StringWriter sw = new StringWriter();
+		PrintWriter pw = new PrintWriter(sw);
+		ex.printStackTrace(pw);
+		String exceptionText = sw.toString();
+
+		Label label = new Label("The exception stacktrace was:");
+
+		TextArea textArea = new TextArea(exceptionText);
+		textArea.setEditable(false);
+		textArea.setWrapText(true);
+
+		textArea.setMaxWidth(Double.MAX_VALUE);
+		textArea.setMaxHeight(Double.MAX_VALUE);
+		GridPane.setVgrow(textArea, Priority.ALWAYS);
+		GridPane.setHgrow(textArea, Priority.ALWAYS);
+
+		GridPane expContent = new GridPane();
+		expContent.setMaxWidth(Double.MAX_VALUE);
+		expContent.add(label, 0, 0);
+		expContent.add(textArea, 0, 1);
+
+		// Set expandable Exception into the dialog pane.
+		getDialogPane().setExpandableContent(expContent);
+	}
+
+	
+}
--- milkman/src/main/java/milkman/MilkmanApplication.java
+++ milkman/src/main/java/milkman/MilkmanApplication.java
@@ -5,35 +5,65 @@ package milkman;
 import java.io.IOException;
 
 import javafx.application.Application;
+import javafx.application.Platform;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Alert.AlertType;
 import javafx.stage.Stage;
+import lombok.SneakyThrows;
+import lombok.extern.slf4j.Slf4j;
 import milkman.ui.main.options.CoreApplicationOptionsProvider;
 
+@Slf4j
 public class MilkmanApplication extends Application {
 	
 	private MainModule module;
 
+	private boolean hasError = false;
+	
 	@Override
 	public void init() throws Exception {
 		super.init();
 		module = new MainModule();
-		module.start();
+		try {
+			module.start();
+		} catch (Throwable t) {
+			hasError = true;
+			Platform.runLater(() -> showFatalError(t));
+		}
 	}
 	
 	@Override
 	public void start(Stage primaryStage) throws Exception {
-		module.getMainWindow().start(primaryStage); // 1 sec
-		module.getApplicationController().initOptions();
-		module.getThemeSwitcher().setTheme(CoreApplicationOptionsProvider.options().getTheme());
-		module.getApplicationController().initApplication(); // 1 sec
-		primaryStage.setOnCloseRequest(e -> module.getApplicationController().persistState());
+		if (!hasError) {
+			try {
+				module.getMainWindow().start(primaryStage); // 1 sec
+				module.getApplicationController().initOptions();
+				module.getThemeSwitcher().setTheme(CoreApplicationOptionsProvider.options().getTheme());
+				module.getApplicationController().initApplication(); // 1 sec
+				primaryStage.setOnCloseRequest(e -> module.getApplicationController().persistState());
+			} catch (Throwable t) {
+				hasError = true;
+				showFatalError(t);
+			}
+		}
 	}
 
 	
 	
-	public static void main(String[] args) throws IOException {
-//		System.out.println("Press Enter to Start..."); // for performance measurements
-//		System.in.read();
+	public static void main(String[] args) throws IOException, InterruptedException {
 		launch(args);
+	}
+
+	@SneakyThrows
+	private static void showFatalError(Throwable t) {
+		try {
+			log.error("Failed to start application", t);
+		} catch (Throwable x){
+			/*we try to log but if it fails (due to permissions maybe), we cant do anything*/
+		}
+		Alert alert = new ExceptionDialog(t);
+		alert.showAndWait();
+		Platform.exit();
 	}
 
 }
