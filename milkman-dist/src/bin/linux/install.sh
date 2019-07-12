f06260b70045dd380ed552858962e9ab3373baa0
Peter Mucha <peter.mucha@sap.com>
2019-07-12 09:38:40 +0200

    made shellscripts executable,added install script for linux

---
+++ milkman-dist/src/bin/linux/install.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+BASE_DIR="$(cd "$(dirname "$0")"; pwd)";
+
+mkdir -p $HOME/.local/share/applications
+
+cat <<EOF > $HOME/.local/share/applications/Milkman.desktop
+[Desktop Entry]
+Name=Milkman
+Exec=$BASE_DIR/milkman.sh
+Icon=$BASE_DIR/milk-bottle.png
+Terminal=false
+Type=Application
+Keywords=milkman;postman;rest;http;sql;jdbc;java;
+EOF
