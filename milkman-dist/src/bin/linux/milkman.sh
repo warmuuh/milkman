c4774bd84fd1d670e05691e20361434a1251fc35
mapuo <mario@softavail.com>
2019-07-28 02:48:27 +0300

    fix starting from other than the base directory

--- milkman-dist/src/bin/linux/milkman.sh
+++ milkman-dist/src/bin/linux/milkman.sh
@@ -1,7 +1,10 @@
 #!/usr/bin/env sh
 
+BASE_DIR="$(cd "$(dirname "$0")"; pwd)" || exit 2
 
-./jre-linux64/bin/java -XX:SharedArchiveFile=app-cds.jsa \
+chmod +x "$BASE_DIR"/jre-linux64/bin/java
+
+"$BASE_DIR"/jre-linux64/bin/java -XX:SharedArchiveFile="$BASE_DIR"/app-cds.jsa \
 	--add-exports javafx.controls/com.sun.javafx.scene.control.behavior=ALL-UNNAMED \
 	--add-exports javafx.controls/com.sun.javafx.scene.control=ALL-UNNAMED \
 	--add-exports javafx.graphics/com.sun.javafx.scene=ALL-UNNAMED \
@@ -12,4 +15,4 @@
 	--add-opens javafx.controls/javafx.scene.control=ALL-UNNAMED \
 	--add-opens javafx.graphics/javafx.scene.text=ALL-UNNAMED \
 	--add-opens javafx.graphics/com.sun.javafx.text=ALL-UNNAMED \
-	-Dprism.dirtyopts=false -cp plugins/*:milkman.jar milkman.MilkmanApplication
+	-Dprism.dirtyopts=false -cp "$BASE_DIR"/plugins/*:"$BASE_DIR"/milkman.jar milkman.MilkmanApplication
