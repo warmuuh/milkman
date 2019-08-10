3603c9e641d0d138dc0a5e60af98eb0c7a36c5cd
Peter Mucha <peter.mucha@sap.com>
2019-08-10 10:55:45 +0200

    used contentEditor for graphql

---
+++ milkman-graphql/src/main/java/milkman/plugin/graphql/GraphqlContentType.java
@@ -0,0 +1,83 @@
+package milkman.plugin.graphql;
+
+import java.util.Collection;
+import java.util.Collections;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+import org.fxmisc.richtext.model.StyleSpans;
+import org.fxmisc.richtext.model.StyleSpansBuilder;
+
+import milkman.ui.plugin.ContentTypePlugin;
+
+public class GraphqlContentType implements ContentTypePlugin {
+	private static final String[] KEYWORDS = new String[] {
+			"query", "mutation", "fragment", "on"
+    };
+
+    private static final String KEYWORD_PATTERN = "\\b(" + String.join("|", KEYWORDS) + ")\\b";
+    private static final String GRAPHQL_KEYWORD = "(?<GRAPHQLKEYWORD>" + KEYWORD_PATTERN + ")";
+    private static final String GRAPHQL_VALUE = "(?<=:)\\s*(?<GRAPHQLVALUE>[\\w]+)";
+    private static final String GRAPHQL_VARIABLE = "(?<GRAPHQLVARIABLE>\\$[\\w]+)";
+    private static final String GRAPHQL_DEFAULT = "(?<==)\\s*(?<GRAPHQLDEFAULT>[^,\\)]+)";
+	private static final String GRAPHQL_BOOL = "(?<GRAPHQLBOOL>true|false)";
+	private static final String GRAPHQL_NUMBER = "(?<GRAPHQLNUMBER>\\d*.?\\d*)";
+
+	    
+	private static final String GRAPHQL_CURLY = "(?<GRAPHQLCURLY>\\{|\\})";
+	private static final String GRAPHQL_ARRAY = "(?<GRAPHQLARRAY>\\[|\\])";
+	private static final Pattern FINAL_REGEX = Pattern.compile(
+			GRAPHQL_KEYWORD
+			+ "|" + GRAPHQL_VALUE 
+			+ "|" + GRAPHQL_VARIABLE
+			+ "|" + GRAPHQL_BOOL 
+			+ "|" + GRAPHQL_DEFAULT
+			+ "|" + GRAPHQL_NUMBER
+			);
+
+
+	
+	@Override
+	public String getName() {
+		return "Graphql";
+	}
+
+	@Override
+	public String getContentType() {
+		return "application/graphql";
+	}
+
+	@Override
+	public boolean supportFormatting() {
+		return false;
+	}
+
+	@Override
+	public String formatContent(String text) {
+		return null;
+	}
+
+	@Override
+	public StyleSpans<Collection<String>> computeHighlighting(String text) {
+		Matcher matcher = FINAL_REGEX.matcher(text);
+        int lastKwEnd = 0;
+        StyleSpansBuilder<Collection<String>> spansBuilder
+                = new StyleSpansBuilder<>();
+        while (matcher.find()) {
+            String styleClass
+                    = matcher.group("GRAPHQLKEYWORD") != null ? "keyword"
+                    : matcher.group("GRAPHQLVALUE") != null ? "value"
+                    : matcher.group("GRAPHQLVARIABLE") != null ? "keyword"
+                    : matcher.group("GRAPHQLDEFAULT") != null ? "value"
+					: matcher.group("GRAPHQLBOOL") != null ? "bool"
+					: matcher.group("GRAPHQLNUMBER") != null ? "number"
+                    : "plain";
+            spansBuilder.add(Collections.emptyList(), matcher.start() - lastKwEnd);
+            spansBuilder.add(Collections.singleton(styleClass), matcher.end() - matcher.start());
+            lastKwEnd = matcher.end();
+        }
+        spansBuilder.add(Collections.emptyList(), text.length() - lastKwEnd);
+        return spansBuilder.create();
+	}
+
+}
--- milkman-graphql/src/main/java/milkman/plugin/graphql/editor/GraphqlAspectEditor.java
+++ milkman-graphql/src/main/java/milkman/plugin/graphql/editor/GraphqlAspectEditor.java
@@ -2,12 +2,14 @@ package milkman.plugin.graphql.editor;
 
 import static milkman.utils.FunctionalUtils.run;
 
+import java.util.Arrays;
+
 import javafx.scene.control.Tab;
-import javafx.scene.control.TextArea;
 import milkman.domain.RequestContainer;
+import milkman.plugin.graphql.GraphqlContentType;
 import milkman.plugin.graphql.domain.GraphqlAspect;
+import milkman.ui.components.ContentEditor;
 import milkman.ui.plugin.RequestAspectEditor;
-import milkman.utils.fxml.GenericBinding;
 
 public class GraphqlAspectEditor implements RequestAspectEditor {
 
@@ -15,15 +17,15 @@ public class GraphqlAspectEditor implements RequestAspectEditor {
 	public Tab getRoot(RequestContainer request) {
 		GraphqlAspect gqlAspect = request.getAspect(GraphqlAspect.class).get();
 		Tab tab = new Tab("Graphql");
-		TextArea textArea = new TextArea();
-		GenericBinding<GraphqlAspect, String> binding = GenericBinding.of(
-				GraphqlAspect::getQuery, 
-				run(GraphqlAspect::setQuery)
-					.andThen(() -> gqlAspect.setDirty(true)), //mark aspect as dirty propagates to the request itself and shows up in UI
-					gqlAspect); 
-		textArea.textProperty().bindBidirectional(binding);
-		textArea.setUserData(binding); //need to add a strong reference to keep the binding from being GC-collected.
-		tab.setContent(textArea);
+		
+		ContentEditor editor = new ContentEditor();
+		editor.setHeaderVisibility(false);
+		editor.setEditable(true);
+		editor.setContentTypePlugins(Arrays.asList(new GraphqlContentType()));
+		editor.setContentType("application/graphql");
+		editor.setContent(gqlAspect::getQuery, run(gqlAspect::setQuery).andThen(() -> gqlAspect.setDirty(true)));
+		
+		tab.setContent(editor);
 		return tab;
 	}
 
