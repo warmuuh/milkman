9ab8a5d29e7f88aac6c72e70fef7493bc461d1e7
Peter Mucha <peter.mucha@sap.com>
2019-09-28 10:07:12 +0200

    fixing logback config. tresholds were ignored

--- milkman/src/main/java/milkman/logback/LogbackConfiguration.java
+++ milkman/src/main/java/milkman/logback/LogbackConfiguration.java
@@ -1,5 +1,6 @@
 package milkman.logback;
 
+import ch.qos.logback.classic.Level;
 import ch.qos.logback.classic.Logger;
 import ch.qos.logback.classic.LoggerContext;
 import ch.qos.logback.classic.PatternLayout;
@@ -10,7 +11,12 @@ import ch.qos.logback.classic.spi.ILoggingEvent;
 import ch.qos.logback.core.ConsoleAppender;
 import ch.qos.logback.core.FileAppender;
 import ch.qos.logback.core.encoder.LayoutWrappingEncoder;
+import ch.qos.logback.core.rolling.FixedWindowRollingPolicy;
+import ch.qos.logback.core.rolling.RollingFileAppender;
+import ch.qos.logback.core.rolling.SizeBasedTriggeringPolicy;
+import ch.qos.logback.core.rolling.TimeBasedRollingPolicy;
 import ch.qos.logback.core.spi.ContextAwareBase;
+import ch.qos.logback.core.util.FileSize;
 import milkman.PlatformUtil;
 
 public class LogbackConfiguration extends ContextAwareBase implements Configurator {
@@ -34,9 +40,16 @@ public class LogbackConfiguration extends ContextAwareBase implements Configurat
 		ConsoleAppender<ILoggingEvent> ca = new ConsoleAppender<ILoggingEvent>();
 		ca.setContext(loggerContext);
 		ca.setName("STDOUT");
+		
+		var filter = new ThresholdFilter();
+		filter.setLevel("INFO");
+		filter.start();
+		ca.addFilter(filter);
+		
 		LayoutWrappingEncoder<ILoggingEvent> encoder = new LayoutWrappingEncoder<ILoggingEvent>();
 		encoder.setContext(loggerContext);
 
+		
 		// same as
 		// PatternLayout layout = new PatternLayout();
 		// layout.setPattern("%d{HH:mm:ss.SSS} [%thread] %-5level %logger{36} - %msg%n");
@@ -46,6 +59,8 @@ public class LogbackConfiguration extends ContextAwareBase implements Configurat
 		layout.start();
 		encoder.setLayout(layout);
 
+		
+		
 		ca.setEncoder(encoder);
 		ca.start();
 
@@ -54,14 +69,38 @@ public class LogbackConfiguration extends ContextAwareBase implements Configurat
 	}
 	
 	private void setupFileAppender(LoggerContext loggerContext) {
-		FileAppender<ILoggingEvent> fa = new FileAppender<ILoggingEvent>();
+		RollingFileAppender<ILoggingEvent> fa = new RollingFileAppender<ILoggingEvent>();
 		fa.setFile(PlatformUtil.getWritableLocationForFile("errors.log"));
+		
+		SizeBasedTriggeringPolicy<ILoggingEvent> sizePolicy = new SizeBasedTriggeringPolicy<ILoggingEvent>();
+		sizePolicy.setContext(loggerContext);
+		sizePolicy.setMaxFileSize(FileSize.valueOf("10mb"));
+		sizePolicy.start();
+		fa.setTriggeringPolicy(sizePolicy);
+		var policy = new FixedWindowRollingPolicy();
+		policy.setMaxIndex(3);
+		policy.setMinIndex(1);
+		policy.setFileNamePattern("errors.%i.log.zip");
+		policy.setParent(fa);
+		policy.setContext(loggerContext);
+		policy.start();
+		fa.setRollingPolicy(policy);
+		
+		
 		fa.setContext(loggerContext);
 		fa.setName("FILE");
 		
+		var filter = new ThresholdFilter();
+		filter.setLevel("ERROR");
+		filter.start();
+		fa.addFilter(filter);
+
 		LayoutWrappingEncoder<ILoggingEvent> encoder = new LayoutWrappingEncoder<ILoggingEvent>();
 		encoder.setContext(loggerContext);
+
 		// same as
+		// PatternLayout layout = new PatternLayout();
+		// layout.setPattern("%d{HH:mm:ss.SSS} [%thread] %-5level %logger{36} - %msg%n");
 		PatternLayout layout = new PatternLayout();
 		layout.setPattern("%date %level [%thread] %logger{10} [%file:%line] %msg%n");
 
@@ -70,9 +109,6 @@ public class LogbackConfiguration extends ContextAwareBase implements Configurat
 		encoder.setLayout(layout);
 		fa.setEncoder(encoder);
 
-		var filter = new ThresholdFilter();
-		filter.setLevel("ERROR");
-		fa.addFilter(filter);
 		
 		fa.start();
 
