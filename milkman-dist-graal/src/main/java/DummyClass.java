d2d23e799f3b91305d2f9281a720d0ee8dfd5a8a
Peter Mucha <peter.mucha@sap.com>
2020-03-06 11:26:18 +0100

    added dummy class

---
+++ milkman-dist-graal/src/main/java/DummyClass.java
@@ -0,0 +1,7 @@
+/**
+ * this class is to have a /target/classes directory to be generated.
+ * Otherwise client-maven-plugin (gluon) fails to package this exact directory
+ */
+public class DummyClass {
+
+}
