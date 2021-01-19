882c34bee157097cc81e475f8a82a4e3d5b026fa
Peter Mucha <pmucha@ebay.com>
2021-01-19 13:47:19 +0100

    handle un-replaced template variables in http exporter. fixes #86

--- milkman-rest/src/main/java/milkman/ui/plugin/rest/curl/HttpTextExport.java
+++ milkman-rest/src/main/java/milkman/ui/plugin/rest/curl/HttpTextExport.java
@@ -1,6 +1,7 @@
 package milkman.ui.plugin.rest.curl;
 
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import milkman.ui.plugin.Templater;
 import milkman.ui.plugin.TextExport;
 import milkman.ui.plugin.rest.domain.HeaderEntry;
@@ -9,8 +10,10 @@ import milkman.ui.plugin.rest.domain.RestHeaderAspect;
 import milkman.ui.plugin.rest.domain.RestRequestContainer;
 import org.apache.commons.lang3.StringUtils;
 
-import java.net.URI;
+import java.net.MalformedURLException;
+import java.net.URL;
 
+@Slf4j
 @RequiredArgsConstructor
 public class HttpTextExport implements TextExport<RestRequestContainer> {
 
@@ -18,36 +21,41 @@ public class HttpTextExport implements TextExport<RestRequestContainer> {
     public String export(boolean isWindows, RestRequestContainer request, Templater templater) {
         StringBuilder b = new StringBuilder();
 
-        var uri = URI.create(templater.replaceTags(request.getUrl()));
+        try {
+            var uri = new URL(templater.replaceTags(request.getUrl()));
 
-        b.append(request.getHttpMethod().toUpperCase())
-                .append(" ");
+            b.append(request.getHttpMethod().toUpperCase())
+                    .append(" ");
 
-        if (StringUtils.isNotBlank(uri.getPath())){
-            b.append(uri.getPath());
-        } else {
-            b.append("/");
+            if (StringUtils.isNotBlank(uri.getPath())) {
+                b.append(uri.getPath());
+            } else {
+                b.append("/");
+            }
+
+            if (StringUtils.isNotBlank(uri.getQuery())) {
+                b.append("?").append(uri.getQuery());
+            }
+            b.append('\n');
+            appendHeader(b, "Host", uri.getHost());
+
+            var headerAspect = request.getAspect(RestHeaderAspect.class).orElseThrow(() -> new IllegalArgumentException("missing aspect"));
+
+            for (HeaderEntry entry : headerAspect.getEntries()) {
+                appendHeader(b, entry.getName(), entry.getValue());
+            }
+
+            var bodyAspect = request.getAspect(RestBodyAspect.class).orElseThrow(() -> new IllegalArgumentException("missing aspect"));
+            if (StringUtils.isNotBlank(bodyAspect.getBody())) {
+                b.append("\n");
+                b.append(bodyAspect.getBody());
+            }
+
+            return templater.replaceTags(b.toString());
+        } catch (MalformedURLException e) {
+            log.warn("failed to export http", e);
         }
-
-        if (StringUtils.isNotBlank(uri.getQuery())) {
-            b.append("?").append(uri.getQuery());
-        }
-        b.append('\n');
-        appendHeader(b, "Host", uri.getHost());
-
-        var headerAspect = request.getAspect(RestHeaderAspect.class).orElseThrow(() -> new IllegalArgumentException("missing aspect"));
-
-        for (HeaderEntry entry : headerAspect.getEntries()) {
-            appendHeader(b, entry.getName(), entry.getValue());
-        }
-
-        var bodyAspect = request.getAspect(RestBodyAspect.class).orElseThrow(() -> new IllegalArgumentException("missing aspect"));
-        if (StringUtils.isNotBlank(bodyAspect.getBody())) {
-            b.append("\n");
-            b.append(bodyAspect.getBody());
-        }
-
-        return templater.replaceTags(b.toString());
+        return "export failed";
     }
 
     private void appendHeader(StringBuilder b,String name, String value) {
