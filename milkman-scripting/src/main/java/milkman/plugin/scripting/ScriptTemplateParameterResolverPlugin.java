a4b7f6ed5ef97191e6f49149eb83a111259c2563
Peter Mucha <pmucha@ebay.com>
2021-03-22 08:00:19 +0100

    fixed active-environment access in script temlpate-resolver plugin

--- milkman-scripting/src/main/java/milkman/plugin/scripting/ScriptTemplateParameterResolverPlugin.java
+++ milkman-scripting/src/main/java/milkman/plugin/scripting/ScriptTemplateParameterResolverPlugin.java
@@ -1,12 +1,17 @@
 package milkman.plugin.scripting;
 
+import milkman.domain.Environment;
 import milkman.domain.RequestExecutionContext;
+import milkman.plugin.scripting.ScriptExecutor.ExecutionResult;
+import milkman.ui.plugin.ActiveEnvironmentAware;
 import milkman.ui.plugin.TemplateParameterResolverPlugin;
 
 import java.util.List;
 import java.util.Optional;
 
-public class ScriptTemplateParameterResolverPlugin implements TemplateParameterResolverPlugin {
+public class ScriptTemplateParameterResolverPlugin implements TemplateParameterResolverPlugin, ActiveEnvironmentAware {
+    private Optional<Environment> activeEnvironment;
+
     @Override
     public String getPrefix() {
         return "js";
@@ -14,7 +19,12 @@ public class ScriptTemplateParameterResolverPlugin implements TemplateParameterR
 
     @Override
     public String lookupValue(String input) {
-        ScriptExecutor.ExecutionResult executionResult = ScriptingAspectPlugin.getGlobalExecutor().executeScript(input, null, null, new RequestExecutionContext(Optional.empty(), List.of()));
+        ExecutionResult executionResult = ScriptingAspectPlugin.getGlobalExecutor().executeScript(input, null, null, new RequestExecutionContext(activeEnvironment, List.of()));
         return executionResult.getResult().orElse("").toString();
+    }
+
+    @Override
+    public void setActiveEnvironment(Optional<Environment> activeEnvironment) {
+        this.activeEnvironment = activeEnvironment;
     }
 }
