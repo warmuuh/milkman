b9ac0ce2ee9b2b1967c4e6d60705ba27e005b209
Peter Mucha <peter.mucha@adevinta.com>
2023-10-30 09:52:22 +0100

    added help button to nosql editor

--- milkman-nosql/src/main/java/milkman/plugin/nosql/editor/NosqlQueryAspectEditor.java
+++ milkman-nosql/src/main/java/milkman/plugin/nosql/editor/NosqlQueryAspectEditor.java
@@ -2,10 +2,17 @@ package milkman.plugin.nosql.editor;
 
 import static milkman.utils.FunctionalUtils.run;
 
+import com.jfoenix.controls.JFXButton;
+import de.jensd.fx.glyphs.fontawesome.FontAwesomeIcon;
+import de.jensd.fx.glyphs.fontawesome.FontAwesomeIconView;
 import java.util.Collections;
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
 import javafx.scene.control.Tab;
+import javafx.scene.layout.StackPane;
 import lombok.SneakyThrows;
 import lombok.val;
+import milkman.PlatformUtil;
 import milkman.domain.RequestContainer;
 import milkman.plugin.nosql.domain.NosqlQueryAspect;
 import milkman.ui.components.ContentEditor;
@@ -19,12 +26,20 @@ public class NosqlQueryAspectEditor implements RequestAspectEditor {
 		val queryAspect = request.getAspect(NosqlQueryAspect.class)
 				.orElseThrow(() -> new IllegalArgumentException("Nosql Query Aspect missing"));
 		
-		ContentEditor root = new ContentEditor();
-		root.setEditable(true);
-		root.setContent(queryAspect::getQuery, run(queryAspect::setQuery).andThen(() -> queryAspect.setDirty(true)));
-		root.setContentTypePlugins(Collections.singletonList(new NosqlContentType()));
-		root.setContentType("application/nosql");
-		return new Tab("Query", root);
+		ContentEditor editor = new ContentEditor();
+		editor.setEditable(true);
+		editor.setContent(queryAspect::getQuery, run(queryAspect::setQuery).andThen(() -> queryAspect.setDirty(true)));
+		editor.setContentTypePlugins(Collections.singletonList(new NosqlContentType()));
+		editor.setContentType("application/nosql");
+
+		JFXButton helpBtn = new JFXButton();
+		helpBtn.setOnAction(e -> PlatformUtil.tryOpenBrowser("https://github.com/eclipse/jnosql/blob/main/COMMUNICATION.adoc#querying-by-text-with-the-communication-api"));
+		helpBtn.setGraphic(new FontAwesomeIconView(FontAwesomeIcon.QUESTION_CIRCLE, "1.5em"));
+
+		editor.addExtraHeaderElement(helpBtn, false);
+
+
+		return new Tab("Query", editor);
 	}
 
 	@Override
